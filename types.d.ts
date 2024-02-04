780b029e4248c0d69c1f8617d2694440b0f21c9a
Nick <ex-er-sis@ya.ru>
2024-02-04 23:13:29 +0300

    Update types and push version

--- types.d.ts
+++ types.d.ts
@@ -1,36 +1,22 @@
-import {
-	getRandomNum as getRndm,
-	roundNumber as roundNum,
-	isString as strCheck,
-	isObject as objCheck,
-	isExistAndNotNull as existCheck,
-	regExp as regExpHelper,
-	forEach as ownForEach,
-	trim as trm,
-	trimAllSpaces as trmAll,
-	capz as cpz,
-	getRandomString as getRndStr,
-} from './source/index.js';
-
 declare type ObjectCheckingType = (value: any) => boolean;
 declare type GetRandomNumType = (min?: number, max?: number) => number;
 declare type RoundNumType = (num: number, prec?: number) => number;
 declare type RegExpHelperType = (name: string) => RegExp;
-declare type ForEachType = <Input>(arr: Input[], fn: (arg: Input) => Void) => Void;
+declare type ForEachType = <Input>(arr: Input[], fn: (arg: Input) => void) => void;
 declare type StringHelperType = (str: string) => string;
 declare type GetRandomStringType = (len: number) => string;
 
-export const isString: ObjectCheckingType<any> = objCheck;
-export const isObject: ObjectCheckingType<any> = strCheck;
-export const isExistAndNotNull: ObjectCheckingType<any> = existCheck;
+export const isString: ObjectCheckingType;
+export const isObject: ObjectCheckingType;
+export const isExistAndNotNull: ObjectCheckingType;
 
-export const getRandomNum: GetRandomNumType<number> = getRndm;
-export const roundNumber: RoundNumType<number> = roundNum;
-export const regExp: RegExpHelperType<string> = regExpHelper;
-export const forEach: ForEachType = ownForEach;
+export const getRandomNum: GetRandomNumType;
+export const roundNumber: RoundNumType;
+export const regExp: RegExpHelperType;
+export const forEach: ForEachType;
 
-export const trim: StringHelperType<string> = trm;
-export const trimAllSpaces: StringHelperType<string> = trmAll;
-export const capz: StringHelperType<string> = cpz;
+export const trim: StringHelperType;
+export const trimAllSpaces: StringHelperType;
+export const capz: StringHelperType;
 
-export const getRandomString: GetRandomStringType<number> = getRndStr;
+export const getRandomString;
